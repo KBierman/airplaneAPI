04a2fe99b0415b6bc110ef82b5218783bb7a5352
Domenico Montalto <montaltodomenico92@gmail.com>
2022-11-10 19:16:47 -0700

    testing pushing

---
+++ airplaneAPI/src/main/java/fish/burger/airplaneapi/UserModel.java
@@ -0,0 +1,21 @@
+package fish.burger.airplaneapi;
+
+public class UserModel {
+
+    private String first_name;
+    private String last_name;
+    private int dayOfBirth;
+    private int monthOfBirth;
+    private int yearOfBirth;
+    private String gender;
+    private String country;
+    private String address;
+    private String city;
+    private String state;
+    private int postal_code;
+    private int phone_number;
+    private String email;
+
+
+
+}
