a95e8ce7b587f8cde257e2d96b8489d0307fd2ac
Domenico Montalto <montaltodomenico92@gmail.com>
2022-11-16 19:29:18 -0700

    added comments in TicketBLL.java

--- airplaneAPI/src/main/java/fish/burger/airplaneapi/TicketBLL.java
+++ airplaneAPI/src/main/java/fish/burger/airplaneapi/TicketBLL.java
@@ -7,17 +7,17 @@ public class TicketBLL {
     //Arraylist of TicketModel
     ArrayList<TicketModel> tickets = new ArrayList();
 
-
-
+    //creates a ticket in the arraylist
     public void createTicket(TicketModel ticket) {
         tickets.add(ticket);
     }
 
+    //reads all tickets in the arraylist
     public ArrayList<TicketModel> readTicket() {
         return tickets;
     }
 
-
+    //updates a ticket in the arraylist
     public void updateTicket(TicketModel ticket) {
         for(int i = 0; i < tickets.size(); i++) {
             if (tickets.get(i).getTicketNo() == ticket.getTicketNo()) {
@@ -27,6 +27,7 @@ public class TicketBLL {
         }
     }
 
+    //deletes a ticket in the arraylist
     public void deleteTicket(int ticketNo) {
         for(int i = 0; i < tickets.size(); i++) {
             if (tickets.get(i).getTicketNo() == ticketNo) {
