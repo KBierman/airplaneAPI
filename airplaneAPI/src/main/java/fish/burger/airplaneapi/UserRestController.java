10c7253209d0d9850433bc8851263f35ee8b294e
Memesterr <98419522+Memesterr@users.noreply.github.com>
2022-11-17 20:01:30 -0700

    Added UserRestController.java and connected it to UserBLL.java

--- airplaneAPI/src/main/java/fish/burger/airplaneapi/UserRestController.java
+++ airplaneAPI/src/main/java/fish/burger/airplaneapi/UserRestController.java
@@ -7,6 +7,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
+    // UserBLL object
     private UserBLL userBLL = new UserBLL();
 
     // Create
