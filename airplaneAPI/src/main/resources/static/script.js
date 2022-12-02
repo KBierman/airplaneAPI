bdcfc6348e144a226f9f105b3fbd520fab87e42c
Memesterr <98419522+Memesterr@users.noreply.github.com>
2022-12-01 18:47:43 -0700

    Working on index.html and script.js integration

--- airplaneAPI/src/main/resources/static/script.js
+++ airplaneAPI/src/main/resources/static/script.js
@@ -10,19 +10,19 @@ function login() {
     password = document.getElementById("text_password").value;
     authHeaderValue = "Basic " + btoa(username + ":" + password); // btoa turns it into base64 encoding
 
-    // XMLHttpRequest to send base64 encoded user login information and then stores it in
+    // XMLHttpRequest to send base64 encoded user login information and then stores it in authHeaderValue
     let xmlHttp = new XMLHttpRequest();
     xmlHttp.open('GET', '');
     xmlHttp.setRequestHeader('Authorization', authHeaderValue);
     xmlHttp.onreadystatechange = function () {
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            console.log("Login Successful!");
-            document.getElementById("response_message").innerHTML = "Welcome, agent " + username;
+            document.getElementById("response_message").innerHTML = "Welcome, " + username;
             isUserLoggedIn(false);
-        } else if (this.readyState === XMLHttpRequest.DONE && this.status === 401) { // When a 401 is returned, do this instead
-            console.log("Login Unsuccessful!")
+            console.log("Login Successful!");
+        } else if (this.readyState === XMLHttpRequest.DONE && this.status === 401) {
             document.getElementById("response_message").innerHTML = "Invalid username/password!";
             isUserLoggedIn(true);
+            console.log("Login Unsuccessful!")
         }
     }
     xmlHttp.send();
@@ -45,50 +45,121 @@ function isUserLoggedIn(isTrue) {
 }
 
 // API methods
-/*
-    - apiMethodName is the name of the method you're calling from the rest controller
-    - requestMethod decides if it's a GET, POST, PUT, DELETE, etc.
-    - apiURL is the mapping for said method from the rest controller
-*/
-function apiMethodNoAuth(apiMethodName, requestMethod, apiURL) {
-    console.log(requestMethod + " " + apiMethodName);
+// Create
+function createFlight() {
+    console.log("Called createFlight()...");
+
+    let flight_number = document.getElementById("").value;
+    let origin = document.getElementById("").value;
+    let destination = document.getElementById("").value;
+    let arrival_time = document.getElementById("").value;
+    let departure_time = document.getElementById("").value;
+    let aircraft_type = document.getElementById("").value;
+    // seats_empty and seats_full are both used to see how full the flight is
+    let seats_empty = document.getElementById("").value;
+    let seats_full = document.getElementById("").value;
+    let fare = document.getElementById("").value; // fare is a double value
+    let flight_date = document.getElementById("").value;
+    // Statuses: delayed, on-time, cancelled, departed, in air, expected, recovery, landed, arrived
+    let flight_status = document.getElementById("").value;
+    let status_date = document.getElementById("").value; // When the status was last updated
+
+    let flight =
+        {
+            "fltNo": flight_number,
+            "origin": origin,
+            "destination": destination,
+            "arrTime": arrival_time,
+            "depTime": departure_time,
+            "aircraftType": aircraft_type,
+            "seatsEmpty": seats_empty,
+            "seatsFull": seats_full,
+            "fare": fare,
+            "flightDate": flight_date,
+            "flightStatus": flight_status,
+            "statusDT": status_date
+        }
+
     let xmlHttp = new XMLHttpRequest();
-    xmlHttp.open(requestMethod, apiURL, true);
+    xmlHttp.open("GET", "/flight", true);
+    xmlHttp.setRequestHeader('Authorization', authHeaderValue);
+    xmlHttp.setRequestHeader("Content-Type", 'application/json');
     xmlHttp.onreadystatechange = function () {
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
             document.getElementById("response_message").innerHTML = this.responseText;
+            console.log("createFlight() was successful!");
+        } else if (this.readyState === XMLHttpRequest.DONE && this.status === 401) { // These statements may not be required
+            document.getElementById("response_message").innerHTML = "You are not logged in and or are unauthorized for creating flights!";
+            console.log("createFlight() failed!");
+        } else if (this.readyState === XMLHttpRequest.DONE && this.status === 403) {
+            document.getElementById("response_message").innerHTML = "You do not have a role for creating flights!";
+            console.log("createFlight() failed!");
         }
     }
-    xmlHttp.send();
+    xmlHttp.send(JSON.stringify(flight));
 }
 
-/*
-    - apiMethodName is the name of the method you're calling from the rest controller
-    - requestMethod decides if it's a GET, POST, PUT, DELETE, etc.
-    - apiURL is the mapping for said method from the rest controller
-*/
-function apiMethodAuth(apiMethodName, requestMethod, apiURL) {
-    console.log(requestMethod + " " + apiMethodName)
+// Read
+function readAllCorrespondingFlights() {
+    console.log("Called readAllCorrespondingFlights()...");
+
+    let origin = document.getElementById("").value;
+    let destination = document.getElementById("").value;
+    let departure_date = document.getElementById("").value;
+
     let xmlHttp = new XMLHttpRequest();
-    xmlHttp.open(requestMethod, apiURL, true);
+    xmlHttp.open("GET", "/flight/" + origin + "/" + destination + "/" + departure_date, true);
     xmlHttp.setRequestHeader('Authorization', authHeaderValue);
     xmlHttp.onreadystatechange = function () {
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
             document.getElementById("response_message").innerHTML = this.responseText;
+            console.log("readAllCorrespondingFlights() was successful!");
         } else if (this.readyState === XMLHttpRequest.DONE && this.status === 401) {
-            document.getElementById("response_message").innerHTML = "You are not logged in and or are unauthorized for: " + apiMethodName;
+            // might not be necessary
+            document.getElementById("response_message").innerHTML = "Sorry, something went wrong! Try again later!";
+            console.log("readAllCorrespondingFlights() failed!");
         } else if (this.readyState === XMLHttpRequest.DONE && this.status === 403) {
-            document.getElementById("response_message").innerHTML = "You do not have a role for: " + apiMethodName;
+            // might not be necessary
+            document.getElementById("response_message").innerHTML = "Sorry, something went wrong! Try again later!";
+            console.log("readAllCorrespondingFlights() failed!");
         }
     }
     xmlHttp.send();
 }
 
+// Update
+
+// Delete
+
 // Page functions
+// Will send user to login page
 function send_to_login() {
     document.getElementById("container").innerHTML =
-    `
+        `
         
     `
 }
 
+// Will send user to their 'purchased' tickets
+function send_to_user_tickets() {
+    document.getElementById("container").innerHTML =
+        `
+        
+    `
+}
+
+// Will send user to the flight reservation page
+function send_to_flight_res() {
+    document.getElementById("container").innerHTML =
+        `
+        
+    `
+}
+
+// Once user has reserved flight, will take them here to show their ticket details
+function send_to_ticket_confirm() {
+    document.getElementById("container").innerHTML =
+        `
+        
+    `
+}
